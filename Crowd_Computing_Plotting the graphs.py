44dbeae91f82bf92a7298faf7bc99e2ff6ca6b41
Aldrin Andrew <andrewjeromias@gmail.com>
2021-06-15 12:49:13 +0530

    Plot graphs using matplotli.pyplot library

---
+++ Crowd_Computing_Plotting the graphs.py
@@ -0,0 +1,19 @@
+"""
+Plotting the graph using matplolib.pyplot
+"""
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4],[1,4,9,16],'ro')  # First list is x axis and second list is y axis
+plt.xlabel("Some Values")
+plt.ylabel("Square Values")
+plt.show()
+
+
+"""
+ro......for red dots
+r--......for red dashes
+bs........for blue squares
+g^........for green triangles
+
+"""
+
