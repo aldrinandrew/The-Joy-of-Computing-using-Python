49daa5ea24c5717c46b105c8f438f6d6761e575e
Aldrin Andrew <andrewjeromias@gmail.com>
2021-06-15 18:37:47 +0530

    Arragned in folder

---
+++ Week 3/Crowd_Computing Plotting graph 2.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+
+import matplotlib.pyplot as plt
+
+Estimates = [50, 25, 27, 38, 63, 92, 15, 7, 3, 56, 42, 38, 35, 55, 51, 60, 75, 99, 70, 80, 89, 76, 60, 44, 42, 43, 51,
+             53, 56, 60, 61, 75, 10, 44, 43, 40, 60, 40, 56]
+
+y = []
+for i in Estimates:
+    y.append(5)
+
+plt.plot(Estimates, y,'ro')
+plt.show()
